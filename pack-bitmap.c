8978014a21c6983d0855b47b001dfba54f87228b
Taylor Blau <me@ttaylorr.com>
2021-09-07 17:17:54 -0400

    t/helper/test-bitmap.c: add 'dump-hashes' mode
    The pack-bitmap writer code is about to learn how to propagate values
    from an existing hash-cache. To prepare, teach the test-bitmap helper to
    dump the values from a bitmap's hash-cache extension in order to test
    those changes.
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pack-bitmap.c
+++ pack-bitmap.c
@@ -1742,6 +1742,28 @@ int test_bitmap_commits(struct repository *r)
 	return 0;
 }
 
+int test_bitmap_hashes(struct repository *r)
+{
+	struct bitmap_index *bitmap_git = prepare_bitmap_git(r);
+	struct object_id oid;
+	uint32_t i;
+
+	if (!bitmap_git->hashes)
+		goto cleanup;
+
+	for (i = 0; i < bitmap_num_objects(bitmap_git); i++) {
+		nth_bitmap_object_oid(bitmap_git, &oid, i);
+
+		printf("%s %"PRIu32"\n",
+		       oid_to_hex(&oid), get_be32(bitmap_git->hashes + i));
+	}
+
+cleanup:
+	free_bitmap_index(bitmap_git);
+
+	return 0;
+}
+
 int rebuild_bitmap(const uint32_t *reposition,
 		   struct ewah_bitmap *source,
 		   struct bitmap *dest)
